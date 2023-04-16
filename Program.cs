9da49dc04147bdbaaf200b94e71e47cd1baa1f45
Mauricio Gracia <mauriciogracia@gmail.com>
2023-04-15 19:09:12 -0500

    Read input from keyboard, and allow more calculations

--- Program.cs
+++ Program.cs
@@ -5,10 +5,27 @@ CitiesList cities = new CitiesList();
 cities.load("./data/ZipCodes.csv");
 //cities.show(); debug data loading
 
-string zipOrigin;
-string zipDestination;
+string ?zipOrigin;
+string ?zipDestination;
+string ?continueCalc;
 
-zipOrigin = "92801";
-zipDestination = "92649";
+do
+{
+    Console.WriteLine("Zip Code Distance Calculation");
 
-Console.WriteLine(cities.CalculateDistance(zipOrigin, zipDestination));
+    do
+    {
+        Console.WriteLine("From:");
+        zipOrigin = Console.ReadLine();
+    } while (string.IsNullOrEmpty(zipOrigin));
+
+    do
+    {
+        Console.WriteLine("To:");
+        zipDestination = Console.ReadLine();
+    } while (string.IsNullOrEmpty(zipDestination));
+
+    Console.WriteLine(cities.CalculateDistance(zipOrigin, zipDestination));
+    Console.WriteLine("Continue ?");
+    continueCalc = Console.ReadLine();
+} while (!string.IsNullOrEmpty(continueCalc) && continueCalc.ToUpper().Equals("Y"));
