d8ae7655e2c970d7f112d69d7c67f045c8d7d185
Mauricio Gracia <mauriciogracia@gmail.com>
2023-04-17 09:39:59 -0500

    Webiste: preparing to call the WebAPI

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 
-function handleSubmit(e:any) {
-    e.preventDefault();
-    console.log('The Console app calculation logic should be a WEB API to be consumed here');
-}
 
 function App() {
-  return (
+    const [zipFrom, setOrig] = useState('');
+    const [zipDest, setDest] = useState('');
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(`call web api ${zipFrom} ${zipDest}`);
+    }
+
+    return (
     <div className="App">
           <form onSubmit={handleSubmit}>
               <label>
                   Zip From:
-                  <input type="text" name="zipFrom" />
+                    <input type="text" name="zipFrom" id="zipFrom" onChange={(event) =>
+                        setOrig(event.target.value)
+                    } />
               </label><br />
               <label>
                   Zip Destination:
-                  <input type="text" name="zipDest" />
+                    <input type="text" name="zipDest" id="zipDest" onChange={(event) =>
+                        setDest(event.target.value)
+                    } />
               </label><br />
               <input type="submit" value="Submit" />
           </form>
